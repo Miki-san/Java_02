12c0189e39789b37ae2d965db2170ad698176bc2
ivc6-10 <mikhael.rykhlov@gmail.com>
2020-09-21 15:39:22 +0300

    :D

---
+++ src/Num_06/Main.java
@@ -0,0 +1,21 @@
+package Num_06;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int field[][] = new int[size][size];
+        for (int i = 0; i < size; i++){
+            for (int j = 0; j < size; j++){
+                field[i][j] = scan.nextInt();
+            }
+        }
+
+
+
+
+
+    }
+}
