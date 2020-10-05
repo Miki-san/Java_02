bc4b7e8f27d5fd88a934470a188d872c804f4341
ivc6-10 <mikhael.rykhlov@gmail.com>
2020-10-05 15:29:20 +0300

    |-(0.0)-|

--- src/Num_10/Main.java
+++ src/Num_10/Main.java
@@ -1,7 +1,12 @@
 package Num_10;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        SwingUtilities.invokeLater(() -> {
+            MySwingApp app = new MySwingApp();
+            app.setVisible(true);
+        });
     }
 }
